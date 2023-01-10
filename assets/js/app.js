74b69a0908de336ed74d6010c5d4c1243b8a38c8
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-01-10 19:00:27 +0100

    Convert the awesome books project from using JS objects to JS classes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,92 +1,146 @@
-function Book(index, bookTitle, bookAuthor) {
-  this.index = index;
-  this.bookTitle = bookTitle;
-  this.bookAuthor = bookAuthor;
+class Book {
+  constructor(bookTitle, bookAuthor) {
+    this.bookTitle = bookTitle;
+    this.bookAuthor = bookAuthor;
+  }
 }
 
-let books = [];
+let books;
 
-// Load book from local storage
-const getBooks = () => {
-  if (localStorage.getItem('books') === null) {
-    localStorage.setItem('books', JSON.stringify(books));
-  } else {
-    books = JSON.parse(localStorage.getItem('books'));
+// SavedBooks Class: Handles Storage
+class SavedBooks {
+  static getBooks() {
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
   }
 
-  return books;
-};
+  static addBook(book) {
+    const books = SavedBooks.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
-// Remove book
-const removeBook = (position) => {
-  const books = getBooks();
-  books.splice(position, 1);
+  static removeBook(bookTitle) {
+    const books = SavedBooks.getBooks();
 
-  localStorage.setItem('books', JSON.stringify(books));
-  window.location.reload();
-};
+    books.forEach((book, index) => {
+      if (book.bookTitle === bookTitle) {
+        books.splice(index, 1);
+      }
+    });
 
-// Clear form fields
-const clearFields = () => {
-  document.querySelector('#bookTitle').value = '';
-  document.querySelector('#bookAuthor').value = '';
-};
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
 
-// Add book
-const addBook = (book) => {
-  const books = getBooks();
-  books.push(book);
-  clearFields();
-  localStorage.setItem('books', JSON.stringify(books));
-  window.location.reload();
-};
+// BookUserInterface Class: Handle BookUserInterface Tasks
+class BookUserInterface {
+  static displayBooks() {
+    const books = SavedBooks.getBooks();
 
-const listBooks = document.querySelector('#book-list');
+    books.forEach((book) => BookUserInterface.addBookToList(book));
+  }
 
-// Display book to UI
-const displayBooks = () => {
-  const books = getBooks();
-  // Load book section dynamically
-  books.forEach((book) => {
+  static addBookToList(book) {
+    const listBooks = document.querySelector('#book-list');
+
     const bookDisplay = document.createElement('div');
     bookDisplay.className = 'bookList1';
     bookDisplay.innerHTML = `
         <p class="bookTitle"><b>${book.bookTitle}</b></p>
-        <p>by<span></span><b>${book.bookAuthor}.</b></p>
-        <button id="${Date.now()}" class="remove">Remove</button>
+        <p class="bookAuthor">&nbsp;by&nbsp;<b>${book.bookAuthor}.</b></p>
+        <button class="remove">Remove</button>
         `;
-    listBooks.appendChild(bookDisplay);
-  });
 
-  localStorage.setItem('books', JSON.stringify(books));
-};
+    listBooks.appendChild(bookDisplay);
+  }
+
+  static removeBook(el) {
+    if (el.classList.contains('remove')) {
+      el.parentElement.remove();
+    }
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static clearFields() {
+    document.querySelector('#bookTitle').value = '';
+    document.querySelector('#bookAuthor').value = '';
+  }
+}
 
 // Event: Display Books
-document.addEventListener('DOMContentLoaded', displayBooks);
+document.addEventListener('DOMContentLoaded', BookUserInterface.displayBooks);
 
 // Event: Add a Book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   // Prevent actual submit
   e.preventDefault();
+
   // Get form values
   const bookTitle = document.querySelector('#bookTitle').value;
   const bookAuthor = document.querySelector('#bookAuthor').value;
-  // Create unique id for each book
-  const index = Date.now();
 
   // Instatiate book
-  const book = new Book(index, bookTitle, bookAuthor);
+  const book = new Book(bookTitle, bookAuthor);
 
   // Add Book to BookUserInterface
-  addBook(book);
+  BookUserInterface.addBookToList(book);
+
+  // Add book to SavedBooks
+  SavedBooks.addBook(book);
+
+  // Clear fields
+  BookUserInterface.clearFields();
 });
 
 // Event: Remove a Book
-listBooks.addEventListener('click', (e) => {
-  // Get the position of DOM parent element of the button clicked
-  const position = Array.from(e.target.parentNode.parentNode.children).indexOf(
-    e.target.parentNode,
-  );
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  // Remove book from BookUserInterface
+  BookUserInterface.removeBook(e.target);
 
-  removeBook(position);
+  // Remove book from SavedBooks
+  SavedBooks.removeBook(
+    e.target.previousElementSibling.previousElementSibling.textContent,
+  );
+});
+
+const currentDate = new Date().toLocaleString();
+document.getElementById('current-date').innerHTML = currentDate;
+
+// display the books list when click the button "List"
+const bookList = document.querySelector('.book-list-container');
+const listBtn = document.querySelector('.listBtn');
+const formContainer = document.querySelector('.form-container');
+
+listBtn.addEventListener('click', () => {
+  bookList.style.display = 'block';
+  formContainer.style.display = 'none';
+});
+
+window.addEventListener('load', () => {
+  bookList.style.display = 'block';
+  formContainer.style.display = 'none';
+});
+
+// display the Add book form  when click the button "Add new"
+const addNewBtn = document.querySelector('.add-new-btn');
+
+addNewBtn.addEventListener('click', () => {
+  bookList.style.display = 'none';
+  formContainer.style.display = 'block';
+});
+
+// display the  Contact section when click the button "Contact"
+const contactBtn = document.querySelector('.contact');
+const contactInfo = document.querySelector('.contact-info');
+
+contactBtn.addEventListener('click', () => {
+  bookList.style.display = 'none';
+  formContainer.style.display = 'none';
+  contactInfo.style.display = 'block';
 });
