55aa847f7a255317aab3de47d3db7e4bec862451
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-01-11 16:33:32 +0100

    Add functionality to the navigation menu in the JS file

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,6 @@ const formContainer = document.querySelector('.form-container');
 const contactBtn = document.querySelector('.contact');
 const contactInfo = document.querySelector('.contact-info');
 
-
 class Book {
   constructor(bookTitle, bookAuthor) {
     this.bookTitle = bookTitle;
@@ -22,7 +21,7 @@ let books;
 // SavedBooks Class: Handles Storage
 class SavedBooks {
   static getBooks() {
-    books = JSON.parse(localStorage.getItem('books')) || []
+    books = JSON.parse(localStorage.getItem('books')) || [];
     return books;
   }
 
@@ -110,7 +109,38 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
   BookUserInterface.removeBook(e.target);
 
   // Remove book from SavedBooks
-  SavedBooks.removeBook(e.target.previousElementSibling.previousElementSibling.textContent);
+  SavedBooks.removeBook(
+    e.target.previousElementSibling.previousElementSibling.textContent
+  );
 });
 
 // Day 3 : Project update
+const currentDate = new Date().toLocaleString();
+document.getElementById('current-date').innerHTML = currentDate;
+
+// display the books list when click the button "List"
+listBtn.addEventListener('click', () => {
+  bookList.style.display = 'block';
+  formContainer.style.display = 'none';
+  contactInfo.style.display = 'none';
+});
+
+window.addEventListener('load', () => {
+  bookList.style.display = 'block';
+  formContainer.style.display = 'none';
+  contactInfo.style.display = 'none';
+});
+
+// display the Add book form  when click the button "Add new"
+addNewBtn.addEventListener('click', () => {
+  bookList.style.display = 'none';
+  formContainer.style.display = 'block';
+  contactInfo.style.display = 'none';
+});
+
+// display the  Contact section when click the button "Contact"
+contactBtn.addEventListener('click', () => {
+  bookList.style.display = 'none';
+  formContainer.style.display = 'none';
+  contactInfo.style.display = 'block';
+});
